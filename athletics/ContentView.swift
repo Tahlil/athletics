fb944ef38a9c9f180c6f6bab04b0cf82d1c9e835
Abu Anwar <abuanwar072@gmail.com>
2021-01-06 15:10:44 +0600

    Initial Commit

---
+++ athletics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  athletics
+//
+//  Created by Abu Anwar MD Abdullah on 6/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ athletics/athleticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  athleticsApp.swift
+//  athletics
+//
+//  Created by Abu Anwar MD Abdullah on 6/1/21.
+//
+
+import SwiftUI
+
+@main
+struct athleticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
